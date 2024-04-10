c9fbb49d30fe05d31068e33c7de1d914c99e9ecc
DanielGCSK8 <danielplus90@gmail.com>
2024-04-09 20:45:24 -0500

    Pagetop y content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageTopComponent } from './page-top/page-top.component';
+import { ContentPageComponent } from './content-page/content-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageTopComponent, ContentPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
